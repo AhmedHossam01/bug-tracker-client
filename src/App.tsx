3c6220b26213f5332cafac90969ec294c231249e
Ahmed Hossam <1318039@gmail.com>
2022-02-02 22:16:31 +0200

    Install TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div className="dark">
+      <div className="text-3xl dark:bg-slate-900 min-h-screen dark:text-slate-100">
+        hello world
+      </div>
+    </div>
+  );
 };
 
 export default App;
