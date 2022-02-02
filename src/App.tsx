e0f2cee36875d3c035eeea5545309c2a82a0925f
Ahmed Hossam <1318039@gmail.com>
2022-02-02 22:27:36 +0200

    Add dark mode support for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { DetailedHTMLProps, HTMLAttributes, useState } from "react";
+
 const App = () => {
+  const [isDark, setIsDark] = useState(true);
+
+  const handleKeyPress = (
+    event: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+  ) => {
+    if (event.key === "Enter") {
+      setIsDark(!isDark);
+    }
+  };
+
   return (
-    <div className="dark">
+    <div
+      className={isDark ? "dark" : ""}
+      onKeyDown={handleKeyPress}
+      tabIndex={0}
+    >
       <div className="text-3xl dark:bg-slate-900 min-h-screen dark:text-slate-100">
         hello world
       </div>
