45e3ae7cb6e3a0a4742ced7cff5700a872a29559
Ahmed Hossam <1318039@gmail.com>
2022-02-03 14:30:37 +0200

    Edit appbar animation

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,10 +1,24 @@
 import { MenuAlt2Icon } from "@heroicons/react/outline";
 
-const Appbar = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
+const Appbar = ({
+  toggleSidebar,
+  isSidebarOpen,
+}: {
+  toggleSidebar: () => void;
+  isSidebarOpen: boolean;
+}) => {
   return (
     <div className="px-6 py-4 bg-white dark:bg-slate-700 dark:text-slate-100 flex justify-between">
       <button onClick={toggleSidebar}>
-        <MenuAlt2Icon className="w-6 h-6" />
+        <div className="w-4 h-3 flex flex-col justify-between">
+          <div className="w-full h-[1.8px] bg-slate-100 rounded-full"></div>
+          <div className="w-full h-[1.8px] bg-slate-100 rounded-full"></div>
+          <div
+            className={`h-[1.8px] bg-slate-100 rounded-full transition-[width] ${
+              isSidebarOpen ? "w-1/2 md:w-full" : "md:w-1/2 w-full"
+            }`}
+          ></div>
+        </div>
       </button>
 
       <div className="avatar">
