2f26ea259e57e529a23ba63078b422527a9267f8
Ahmed Hossam <1318039@gmail.com>
2022-02-06 16:14:56 +0200

    Build notification dropdown

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,3 +1,5 @@
+import { BellIcon } from "@heroicons/react/outline";
+
 const Appbar = ({
   toggleSidebar,
   isSidebarOpen,
@@ -19,12 +21,41 @@ const Appbar = ({
         </div>
       </button>
 
-      <div className="avatar">
-        <div className="h-10 w-10 mask mask-squircle">
-          <img
-            src="https://avatars.githubusercontent.com/u/50014916?v=4"
-            alt="profile"
-          />
+      <div className="flex items-center gap-x-2">
+        <div className="dropdown dropdown-end">
+          <button tabIndex={0} className="btn btn-square btn-ghost">
+            <BellIcon className="w-6 h-6" />
+          </button>
+
+          <ul
+            tabIndex={0}
+            className="p-2 shadow menu dropdown-content bg-base-100 rounded-box hidden flex-col gap-y-4 md:flex md:w-[30rem] overflow-auto max-h-56"
+          >
+            <li className="flex justify-between p-1 flex-row items-center gap-x-3">
+              <div className="avatar">
+                <div className="rounded-btn w-10 h-10">
+                  <img
+                    src="https://avatars.githubusercontent.com/u/50014916?v=4"
+                    alt="Ahmed  Hossam"
+                  />
+                </div>
+              </div>
+              <p>Ahmed Hossam sent you an invitation to project</p>
+              <div className="flex-none">
+                <button className="btn btn-sm btn-ghost mr-2">Cancel</button>
+                <button className="btn btn-sm btn-primary">Approve</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div className="avatar">
+          <div className="h-10 w-10 mask mask-squircle">
+            <img
+              src="https://avatars.githubusercontent.com/u/50014916?v=4"
+              alt="profile"
+            />
+          </div>
         </div>
       </div>
     </div>
