481e2c10e6daffe6cbfc3f5d05a27de3bbee9e09
Ahmed Hossam <1318039@gmail.com>
2022-02-14 13:52:27 +0200

    Fix dynamic styling issue

--- src/components/Dashboard/ProjectCard.tsx
+++ src/components/Dashboard/ProjectCard.tsx
@@ -9,7 +9,8 @@ const DashboardProjectCard = ({
 }) => {
   return (
     <div
-      className={`bg-${color}-500 text-white p-2 rounded-md h-24 text-4xl truncate flex flex-col justify-between`}
+      className={`text-white p-2 rounded-md h-24 text-4xl truncate flex flex-col justify-between`}
+      style={{ backgroundColor: color }}
     >
       {name}
       <div className="text-gray-100 text-sm mt-3 flex items-center gap-x-1">
--- src/components/Sidebar/SubLink.tsx
+++ src/components/Sidebar/SubLink.tsx
@@ -1,15 +1,20 @@
 import ProjectInterface from "../../types/ProjectInterface";
 
-const SubLink = ({ project }: { project: ProjectInterface }) => {
+const SubLink = ({
+  project: { color, name },
+}: {
+  project: ProjectInterface;
+}) => {
   return (
     <li className="my-px">
       <div
         className={`flex flex-row items-center h-12 px-4 rounded-lg text-gray-600 dark:text-gray-200`}
       >
         <span
-          className={`w-2 h-2 rounded-full bg-${project.color}-500 ml-3`}
+          className={`w-2 h-2 rounded-full ml-3`}
+          style={{ backgroundColor: color }}
         ></span>
-        <span className="ml-4">{project.name}</span>
+        <span className="ml-4 truncate">{name}</span>
       </div>
     </li>
   );
