0895c010686a1f51aa3e996b9dd86b6028830f97
Ahmed Hossam <1318039@gmail.com>
2022-02-16 15:02:03 +0200

    Re-design project card

--- src/components/Dashboard/ProjectCard.tsx
+++ src/components/Dashboard/ProjectCard.tsx
@@ -8,14 +8,18 @@ const DashboardProjectCard = ({
   project: ProjectInterface;
 }) => {
   return (
-    <div
-      className={`text-white p-2 rounded-md h-24 text-4xl truncate flex flex-col justify-between`}
-      style={{ backgroundColor: color }}
-    >
-      {name}
-      <div className="text-gray-100 text-sm mt-3 flex items-center gap-x-1">
-        <ClockIcon className="w-4 h-4" />
-        Updated {calcTimeAgo(updated_at)}
+    <div className="group relative shadow-lg dark:shadow-slate-800 bg-slate-100 dark:bg-slate-800 dark:text-white rounded-sm text-slate-700 px-4 py-2 text-4xl flex flex-col justify-between">
+      <div
+        className="ease-out absolute h-full top-0 left-0 w-2 transition-[width] duration-300 group-hover:w-full"
+        style={{ backgroundColor: color }}
+      ></div>
+      <div className="group-hover:text-white z-10 duration-300">
+        {name.substring(0, 15)}
+        {name.length > 15 && "..."}
+        <div className="group-hover:text-white duration-300 transition-colors dark:text-gray-400 text-gray-500 text-sm mt-3 flex items-center gap-x-1">
+          <ClockIcon className="w-4 h-4" />
+          Updated {calcTimeAgo(updated_at)}
+        </div>
       </div>
     </div>
   );
